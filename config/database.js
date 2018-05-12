058911c171d7f3893f6792946356e226e9fdf21d
N03499107 <patelr13@hawkmail.newpaltz.edu>
2018-05-12 15:59:17 -0400

    Register works with postman!

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/Exercise',
-    secret: 'yoursecret'
-  }
-  
+  database: 'mongodb://localhost:27017/exercise',
+  secret: 'yoursecret'
+}
